4185f3aee35fc5300fb4cbb125134fe1299ad3a5
Ryan Williams <rwilliams@stream9.net>
2023-06-02 22:45:06 -0400

    Updated cli application

--- src/main.c
+++ src/main.c
@@ -2,29 +2,27 @@
 #include <stdio.h>
 #include <assert.h>
 #include <string.h>
-#include <libgen.h>
 
-#include "stringview.h"
+#include "file.h"
+#include "tokenizer.h"
+#include "parser.h"
+#include "interpreter.h"
 
-
-#define USAGE_BUFFER_SIZE 2048
-char cm_usage_buffer[USAGE_BUFFER_SIZE] = {0};
+#define CM_EXIT_USAGE 1
 
 
 const char* cm_get_usage ()
 {
-	char usage_file_path[256] = {0};
-
-    snprintf(usage_file_path, sizeof(usage_file_path), "%s%s", dirname(__FILE__), "/../usage.txt");
-	FILE * usage_file = fopen(usage_file_path, "r");
-
-	assert(usage_file && "Unable to open usage.txt\n");
-
-	size_t bytes_read = fread(cm_usage_buffer, sizeof(cm_usage_buffer), 1, usage_file);
-
-	assert(usage_file && "Zero bytes read from usage.txt\n");
-
-	return (const char *) cm_usage_buffer;
+	return
+		"\n"
+		"OVERVIEW: A utility for working with cogmech files\n"
+		"\n"
+		"USAGE: cogm <subcommand> [args]\n"
+		"\n"
+		"SUBCOMMANDS:\n"
+		"  tokenize <file>       Tokenizes a valid .cogm <file>\n"
+		"  parse <file>          Parses a valid .cogm <file> into an AST\n"
+		"  exec <file>           Parses and executes a valid .cogm <file>\n";
 }
 
 
@@ -32,11 +30,50 @@ void cm_print_usage ()
 {
 	const char *usage = cm_get_usage();
 	printf("%s\n", usage);
+	exit(CM_EXIT_USAGE);
+}
+
+
+const char *cm_shift_arg (int *argc, const char ***argv)
+{
+	assert(argc > 0);
+
+	const char *value = (*argv)[0];
+
+	(*argc) -= 1;
+	(*argv) += 1;
+
+	return value;
 }
 
 
 int main (int argc, const char **argv)
 {
-	cm_print_usage();
+	const char *program = cm_shift_arg(&argc, &argv);
+
+	if (argc < 1) {
+		cm_print_usage();
+	}
+
+	const char *subcommand = cm_shift_arg(&argc, &argv);
+	const char *filepath = cm_shift_arg(&argc, &argv);
+
+	if (strcmp(subcommand, "tokenize") == 0) {
+		CMTokenList list = cm_tokenize_file(filepath);
+		cm_print_tokenlist(list);
+
+	} else if (strcmp(subcommand, "parse") == 0) {
+		CMTokenList list = cm_tokenize_file(filepath);
+		CMNode *ast = cm_parse_file(&list);
+		cm_print_node(ast);
+
+	} else if (strcmp(subcommand, "exec") == 0) {
+		CMTokenList list = cm_tokenize_file(filepath);
+		CMNode *ast = cm_parse_file(&list);
+		CMContext context = cm_context();
+
+		cm_interpret(&context, ast);
+	}
+
 	return 0;
 }
